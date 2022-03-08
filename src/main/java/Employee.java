5528aef56657b054eace46ed0b1d5f16dce9d5b8
Alex <G00372513@gmit.ie>
2022-03-08 09:34:28 +0000

    Added code into Employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,12 @@
 public class Employee {
+    private String Title;
+    private String Name;
+    private String PPS;
+    private int Phone;
+    private String Type;
+    private int Age;
+
+    public Employee(String title, String name, String pps, int phone, String type, int age){
+
+    }
 }
