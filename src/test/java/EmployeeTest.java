0d2a0a090e5c6ea1d44bb43bf945f268971e3362
Alex <G00372513@gmit.ie>
2022-03-08 10:43:53 +0000

    Added test to each constructor

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -8,36 +8,42 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class EmployeeTest {
     private Employee Emp;
 
+    @Test
     public void TestConstructorInvalidTitle(){
         String invalid = "Invalid title";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("","Alex","22150PO2",123456789,"Full-Time",22));
         assertEquals(invalid, exception.getMessage());
     }
 
+    @Test
     public void TestConstructorInvalidName(){
         String invalid = "Invalid name";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("Mr","","22150PO2",123456789,"Full-Time",22));
         assertEquals(invalid, exception.getMessage());
     }
 
+    @Test
     public void TestConstructorInvalidPPS(){
         String invalid = "PPS not valid";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("Mr","Alex","",123456789,"Full-Time",22));
         assertEquals(invalid, exception.getMessage());
     }
 
+    @Test
     public void TestConstructorInvalidType(){
         String invalid = "Employment type incorrect";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("Mr","Alex","22150PO2",123456789,"",22));
         assertEquals(invalid, exception.getMessage());
     }
 
+    @Test
     public void TestConstructorInvalidAge(){
         String invalid = "Age must be over 16";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("Mr","Alex","22150PO2",123456789,"Full-Time",12));
         assertEquals(invalid, exception.getMessage());
     }
 
+    @Test
     public void TestConstructorInvalidPhone(){
         String invalid = "Phone number must be a nine digit number";
         Exception exception = assertThrows(IllegalArgumentException.class, ()->new Employee("Mr","Alex","22150PO2",1234567,"Full-Time",22));
